a19f3aeac39bf5be82865c2cf3e5e8ece89df5dd
Felipe <duskmariner@gmail.com>
2017-12-21 08:55:22 -0200

    material converter e material cadastro ok

---
+++ CMDWeb/src/br/com/cmdweb/converter/FabricanteConverter.java
@@ -0,0 +1,44 @@
+package br.com.cmdweb.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.com.cmdweb.DAO.FabricanteDAO;
+import br.com.cmdweb.domain.Fabricante;
+
+@FacesConverter("fabricanteConverter")
+public class FabricanteConverter implements Converter{
+
+	@Override
+	public Object getAsObject(FacesContext facescontext, UIComponent component, String valor) {
+		
+		try
+		{
+			Long codigo = Long.parseLong(valor);
+			FabricanteDAO dao = new FabricanteDAO();
+			Fabricante fab = dao.BuscarporCodigo(codigo);
+			return fab;
+		}
+		catch(RuntimeException e)
+		{
+			return null;
+		}
+	}
+
+	@Override
+	public String getAsString(FacesContext facescontext, UIComponent component, Object objeto) {
+		try
+		{
+			Fabricante fab = (Fabricante) objeto;
+			Long codigo = fab.getCodfabricante();
+			return codigo.toString();
+		}
+		catch(RuntimeException e)
+		{
+			return null;
+		}
+	}
+
+}
